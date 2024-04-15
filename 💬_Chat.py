d99a09fe45ad98095825970de0f0e68e8410c553
jhicks2306 <jhicks2306@gmail.com>
2024-04-15 10:25:05 +0100

    Restore chat history to Chat.py

--- 💬_Chat.py
+++ 💬_Chat.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from langchain_community.llms import Ollama
 from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_core.tools import tool
@@ -8,18 +7,11 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-from utils.load_data import create_vector_db
-from utils.lemmatizer import lemmatize_sentence
-from router import get_routing_chain
-import json
 from food_list import FoodList
-from sql_chain import get_query_chain
 from project_config import ProjectConfig
-from datetime import date, timedelta
 from dotenv import dotenv_values
 from operator import itemgetter
 import os
-from langchain_core.messages import HumanMessage, AIMessage
 
 env_vars = dotenv_values(".env")
 os.environ["LANGCHAIN_API_KEY"] = env_vars['LANGCHAIN_API_KEY']
@@ -37,13 +29,10 @@ if len(msgs.messages) == 0:
     msgs.add_ai_message("PantryPal: How can I help you?")
 config = {"configurable": {"session_id": "any"}}
 
-# Set up chain to route user messages to the correct response.
-router = get_routing_chain()
-
 # Set up main chat model chain and pass message history.
 model = ChatOllama(model='mistral:instruct')
 
-# Define tools available.
+# Define tools for interacting with database.
 @tool
 def add(ingredients: list) -> str:
     """Add list of food ingredients e.g. ['apples', 'pasta'] when the user buys food)."""
@@ -56,14 +45,35 @@ def remove(ingredients: list) -> str:
     response = pantry.delete_items(ingredients)
     return response
 
+# Define tool 
+chat_prompt = ChatPromptTemplate.from_messages( 
+    [
+        ("system", "You are a helpful assistant called PantryPal. You are knowledgable about food ingedients, recipes and cooking. Provide a natural language response and be concise. Start each response with PantryPal: "),
+        MessagesPlaceholder(variable_name="history"),
+        ("user", """
+        Answer the following query with reference to the list of ingredients available in the pantry below. 
+         
+        Ingredients available in the pantry: {pantry_items}
+        Query : {input}
+        """)
+    ]
+)
+
+# Get list of pantry items for the prompt.
+pantry_items = pantry.to_string()
+
+# Chain for when the response is a general query about the pantry.
+chat_chain = chat_prompt | model | StrOutputParser()
+
 @tool
 def converse(input: str) -> str:
     "Provide a natural language response using the user input."
-    return model.invoke(input).content
+    params = {"input": input, "pantry_items": pantry_items, "history": msgs.messages}
+    return chat_chain.invoke(params).content
 
 tools = [add, remove, converse]
 
-# Configure the system prompts
+# Configure the system prompt for the chooser LLM.
 rendered_tools = render_text_description(tools)
 
 system_prompt = f"""You are an assistant that has access to the following set of tools. Here are the names and descriptions for each tool:
@@ -71,7 +81,7 @@ system_prompt = f"""You are an assistant that has access to the following set of
 {rendered_tools}
 
 Given the user input, return the name and input of the tool to use. Return your response as a JSON blob with 'name' and 'arguments' keys. The value associated with the 'arguments' key should be a dictionary of tool parameters.
-Do not include the '\' character in the response."""
+Do not include the '\' character anywhere in the response."""
 
 prompt = ChatPromptTemplate.from_messages(
     [("system", system_prompt), ("user", "{input}")]
@@ -84,32 +94,6 @@ def tool_chain(model_output):
     return itemgetter("arguments") | chosen_tool
 
 chooser = prompt | model | JsonOutputParser()
-
-# prompt = ChatPromptTemplate.from_messages( 
-#     [
-#         ("system", "You are a helpful assistant called PantryPal. You are knowledgable about food ingedients, recipes and cooking. Provide a natural language response and be concise. Start each response with PantryPal: "),
-#         MessagesPlaceholder(variable_name="history"),
-#         ("user", """
-#         Answer the following query with reference to the list of ingredients available in the pantry below. 
-         
-#         Ingredients available in the pantry: {pantry_items}
-#         Query : {input}
-#         """)
-#     ]
-# )
-
-# pantry_items = pantry.to_string()
-
-# # Chain for when the response comes from the chat LLM.
-# chain = prompt | model
-# chain_with_history = RunnableWithMessageHistory(
-#     chain,
-#     lambda session_id: msgs,
-#     input_messages_key="input",
-#     history_messages_key="history"
-# )
-
-
 
 # Define utility function for streaming responses.
 def string_to_generator(input_string):
